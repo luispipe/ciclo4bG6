8195e41606cd4662ac9d1b70b99c7d61b4d7cee5
luispipe <lflondonor@unal.edu.co>
2022-10-28 16:11:46 -0500

    Second commit

--- app/src/main/java/com/example/unleeg6/Views/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/unleeg6/Views/ui/activities/LoginActivity.kt
@@ -2,13 +2,15 @@ package com.example.unleeg6.Views.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.unleeg6.R
 import com.example.unleeg6.databinding.ActivityMainBinding
 
 class LoginActivity: AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+      //  binding= ActivityMainBinding.inflate(layoutInflater)
+      //  setContentView(binding.root)
+        setContentView(R.layout.activity_login)
     }
 }
--- app/src/main/java/com/example/unleeg6/Views/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/unleeg6/Views/ui/activities/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.unleeg6.Views.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import com.example.unleeg6.R
 import com.example.unleeg6.databinding.ActivityMainBinding
 
@@ -20,6 +22,12 @@ class MainActivity : AppCompatActivity() {
         //binding.animationView.setAnimation(R.raw.book_animation)
         binding.animationView.playAnimation()
         //Trancisión entre la animación y la siguiente activity (4S)
+        handler= Handler(Looper.myLooper()!!)
+        handler.postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        },4000)
     }
 }
 
