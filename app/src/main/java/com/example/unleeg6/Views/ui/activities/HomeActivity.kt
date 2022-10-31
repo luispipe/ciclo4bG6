a4f530667b14f1b2ac3ec679083d4c2ad6ae8d26
luispipe <lflondonor@unal.edu.co>
2022-10-31 16:45:35 -0500

    menus

--- app/src/main/java/com/example/unleeg6/Views/ui/activities/HomeActivity.kt
+++ app/src/main/java/com/example/unleeg6/Views/ui/activities/HomeActivity.kt
@@ -1,12 +1,22 @@
 package com.example.unleeg6.Views.ui.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.example.unleeg6.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity: AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+       val inflater: MenuInflater= menuInflater
+        inflater.inflate(R.menu.bnv_menu, menu)
+        return true
     }
 }
