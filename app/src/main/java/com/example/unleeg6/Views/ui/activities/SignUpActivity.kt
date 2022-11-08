45b203e9aba971c3a23cde47696b3a78cebc51e2
luispipe <lflondonor@unal.edu.co>
2022-11-08 17:02:37 -0500

    firebase

--- app/src/main/java/com/example/unleeg6/Views/ui/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/unleeg6/Views/ui/activities/SignUpActivity.kt
@@ -3,17 +3,42 @@ package com.example.unleeg6.Views.ui.activities
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.unleeg6.R
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity: AppCompatActivity(){
     lateinit var home: Button
+    lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        firebaseAuth= Firebase.auth
         home= findViewById(R.id.register)
+        val name= findViewById<EditText>(R.id.signupName)
+        val email= findViewById<EditText>(R.id.signupEmail)
+        val birthday= findViewById<EditText>(R.id.signupBirthday)
+        val password= findViewById<EditText>(R.id.signupPassword)
         home.setOnClickListener {
-            startActivity(Intent(this,HomeActivity::class.java))
+            createUser(email.text.toString(),password.text.toString())
+        }
+    }
+    private fun createUser(email:String, password:String){
+        firebaseAuth.createUserWithEmailAndPassword(email,password).
+        addOnCompleteListener(this){
+            Task->if (Task.isSuccessful){
+                startActivity(Intent(this,LoginActivity::class.java))
+            }
+            else{
+                Toast.makeText(applicationContext,"No se puede registrar este usuario",Toast.LENGTH_LONG).show()
+
+            }
         }
     }
 }
