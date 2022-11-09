7efee42d9bb8f6e25e6de706a444842185a52cd8
luispipe <lflondonor@unal.edu.co>
2022-11-09 16:44:18 -0500

    firebase Auth

--- app/src/main/java/com/example/unleeg6/Views/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/unleeg6/Views/ui/activities/LoginActivity.kt
@@ -2,24 +2,36 @@ package com.example.unleeg6.Views.ui.activities
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.example.unleeg6.R
 import com.example.unleeg6.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 class LoginActivity: AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var signup: Button
     lateinit var home: Button
+    lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
       //  binding= ActivityMainBinding.inflate(layoutInflater)
       //  setContentView(binding.root)
         setContentView(R.layout.activity_login)
+
+        firebaseAuth= Firebase.auth
+        val email= findViewById<EditText>(R.id.loginEmail)
+        val password= findViewById<EditText>(R.id.loginPassword)
+
         home= findViewById(R.id.login)
         signup= findViewById(R.id.signup)
         home.setOnClickListener {
-            startActivity(Intent(this, HomeActivity::class.java))
+                login(email.text.toString(),password.text.toString())
         }
         signup.setOnClickListener {
             startActivity(Intent(this,SignUpActivity::class.java))
@@ -27,8 +39,18 @@ class LoginActivity: AppCompatActivity() {
 
         val txtNewPass= findViewById<TextView>(R.id.textViewForgotPassword)
         txtNewPass.setOnClickListener {
-            startActivity(Intent(this,SignUpActivity::class.java))
+            startActivity(Intent(this,RestoreActivity::class.java))
         }
 
+    }
+    fun login(email:String,password:String){
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                Task->if(Task.isSuccessful){
+                startActivity(Intent(this, HomeActivity::class.java))
+                }else{
+                   Toast.makeText(baseContext,"ERROR",Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
