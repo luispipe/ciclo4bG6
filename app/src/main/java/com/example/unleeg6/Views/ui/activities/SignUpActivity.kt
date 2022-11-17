39b79ec8e539756e41221fe1d2044bfa4c0aacd1
luispipe <lflondonor@unal.edu.co>
2022-11-17 16:53:10 -0500

    Maps-2

--- app/src/main/java/com/example/unleeg6/Views/ui/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/unleeg6/Views/ui/activities/SignUpActivity.kt
@@ -11,28 +11,41 @@ import com.example.unleeg6.R
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.FirebaseDatabase
 
 class SignUpActivity: AppCompatActivity(){
     lateinit var home: Button
     lateinit var firebaseAuth: FirebaseAuth
+    lateinit var dbReference: DatabaseReference
+    lateinit var database:FirebaseDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         firebaseAuth= Firebase.auth
+        database= FirebaseDatabase.getInstance()
+        dbReference= database.reference.child("User")
         home= findViewById(R.id.register)
         val name= findViewById<EditText>(R.id.signupName)
         val email= findViewById<EditText>(R.id.signupEmail)
         val birthday= findViewById<EditText>(R.id.signupBirthday)
         val password= findViewById<EditText>(R.id.signupPassword)
         home.setOnClickListener {
-            createUser(email.text.toString(),password.text.toString())
+            createUser(email.text.toString(),password.text.toString(),
+                name.text.toString(),birthday.text.toString())
         }
     }
-    private fun createUser(email:String, password:String){
+    private fun createUser(email:String, password:String, name:String, birthday:String){
         firebaseAuth.createUserWithEmailAndPassword(email,password).
         addOnCompleteListener(this){
             Task->if (Task.isSuccessful){
+                val user=firebaseAuth.currentUser
+                val userdb= dbReference.child(user?.uid.toString())
+                userdb.child("name").setValue(name)
+                userdb.child("birthday").setValue(birthday)
+
                 startActivity(Intent(this,LoginActivity::class.java))
             }
             else{
--- app/src/main/java/com/example/unleeg6/Views/ui/fragments/ShippingFragment.kt
+++ app/src/main/java/com/example/unleeg6/Views/ui/fragments/ShippingFragment.kt
@@ -1,10 +1,16 @@
 package com.example.unleeg6.Views.ui.fragments
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.unleeg6.BuildConfig
 import com.example.unleeg6.R
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -13,6 +19,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
+import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.views.MapView
 import org.osmdroid.util.GeoPoint
@@ -22,6 +29,11 @@ class ShippingFragment : Fragment(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
 
     lateinit var map: MapView
+
+    companion object{
+        const val REQUEST_CODE_LOCATION=0
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,8 +45,10 @@ class ShippingFragment : Fragment(), OnMapReadyCallback {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-      /*  val mapFragment= childFragmentManager.findFragmentById(R.id.mapGoogle) as SupportMapFragment
-        mapFragment.getMapAsync(this)*/
+        val mapFragment= this.childFragmentManager.findFragmentById(R.id.mapGoogle) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID)
 
         //Open Street Map
         map= view.findViewById(R.id.mapOpen)
@@ -59,13 +73,38 @@ class ShippingFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
 
         // Add a marker in Colombia and move the camera
-      /*  val colombia = LatLng(5.070275,-75.513817)
+       val colombia = LatLng(5.070275,-75.513817)
         googleMap?.let {
             this.mMap=it
             googleMap.addMarker(MarkerOptions().position(colombia))
-        }*/
+        }
+
+        enableLocation()
      }
 
+    @SuppressLint("MissingPermission")
+    fun enableLocation(){
+        if(!::mMap.isInitialized) return
+            if (isLocationPermissionGrated()){
+                mMap.isMyLocationEnabled= true
+            }else{
+                requestLocationPermission()
+            }
+    }
 
+    fun isLocationPermissionGrated()=ContextCompat.checkSelfPermission(
+        this.requireContext(),
+        android.Manifest.permission.ACCESS_FINE_LOCATION
+    )== PackageManager.PERMISSION_GRANTED
 
+    fun requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this.requireActivity(),android.Manifest.permission.ACCESS_FINE_LOCATION)){
+            Toast.makeText(this.context, "Requiere activar permisos en los ajustes del celular",Toast.LENGTH_LONG).show()
+
+        }else{
+            ActivityCompat.requestPermissions(this.requireActivity(), arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+             com.example.unleeg6.Views.ui.fragments.ShippingFragment.Companion.REQUEST_CODE_LOCATION
+                )
+        }
+    }
 }
